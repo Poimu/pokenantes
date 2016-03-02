9304d83b12b64eff45b455f46a61b07ad6f41ba0
mohcine <Stagiaire@PCP-07>
2016-03-02 15:17:40 +0100

    f

--- pokenantes/src/main/java/com/pokenantes/actions/DeleteProduct.java
+++ pokenantes/src/main/java/com/pokenantes/actions/DeleteProduct.java
@@ -14,35 +14,66 @@ import com.pokenantes.dtos.Article;
 @Action("/deleteProduct")
 @ParentPackage("json-default")
 @Result(type = "json", params = { "msg" })
-public class DeleteProduct extends ActionSupport{
-	
+public class DeleteProduct extends ActionSupport {
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	int id ;
-	
+	int id;
+
 	private ArrayList<Article> productsList;
 
 	/* Les variables que l'on va renvoyer */
 	private String msg;
-	
+
 	@Autowired
 	private Dao dao;
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public ArrayList<Article> getProductsList() {
+		return productsList;
+	}
+
+	public void setProductsList(ArrayList<Article> productsList) {
+		this.productsList = productsList;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Dao getDao() {
+		return dao;
+	}
+
+	public void setDao(Dao dao) {
+		this.dao = dao;
+	}
+
 	public String execute() {
-		
-		if (dao.findById(id)!=null){
+
+		if (dao.findById(id) != null) {
 			System.out.println("Article à supprimer trouvé ");
 			dao.deleteArticle(id);
-			msg="succes";
-			
-		}else 
-			msg="echec";
-		
-		
+			msg = "succes";
+
+		} else
+			msg = "echec";
+
 		return ActionSupport.SUCCESS;
 	}
-	
+
 }
